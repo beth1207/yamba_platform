16eef4ce5d3f9c61439cac2e62cfca36da4358ad
beth <132913489+beth1207@users.noreply.github.com>
2025-06-16 14:47:00 +0300

    Delete yamba_backend directory

--- yamba_backend/jobs/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import JobListCreateView
-
-urlpatterns = [
-    path('jobs/', JobListCreateView.as_view(), name='job-list-create'),
-]
--- yamba_backend/jobs/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics
-from .models import Job
-from .serializers import JobSerializer
-
-class JobListCreateView(generics.ListCreateAPIView):
-    queryset = Job.objects.all()
-    serializer_class = JobSerializer
-
